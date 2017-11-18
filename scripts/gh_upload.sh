707c73e0d67aa4eb1f99ba6be52299a13d791525
Dávid Szakállas <david.szakallas@gmail.com>
2017-11-18 17:31:26 +0100

    gh-upload fix

--- scripts/gh_upload.sh
+++ scripts/gh_upload.sh
@@ -2,13 +2,13 @@
 set -e # exit with nonzero exit code if anything fails
 
 # create temporary directory to work in
-GITDIR=$(mktemp -d); trap "{rm -rf $GITDIR}" EXIT
+GITDIR=$(mktemp -d); trap "{ rm -rf $GITDIR; }" EXIT
 
 # run our compile script, discussed above
 cp $OUTDIR/*.pdf $GITDIR
 
 # go to the out directory and create a *new* Git repo
-cd $GIDIR
+cd $GITDIR
 git init
 
 # inside this git repo we'll pretend to be a new user
@@ -24,4 +24,4 @@ git commit -m "Deploy to GitHub Pages"
 # repo's gh-pages branch. (All previous history on the gh-pages branch
 # will be lost, since we are overwriting it.) We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-git push --force --quiet "https://${GH_TOKEN}@github.com/${GH_REPO}.git" $BRANCH:$TGT_BRANCH > /dev/null 2>&1
+git push --force --quiet "https://${GH_TOKEN}@github.com/${GH_REPO}.git" $BRANCH:$TGT_BRANCH  > /dev/null 2>&1
